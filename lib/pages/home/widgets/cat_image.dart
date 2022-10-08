54a46487c7c84c1871e4045660d0367844c3463a
Donquixot228 <64096633+Donquixot228@users.noreply.github.com>
2022-10-08 16:54:14 +0300

    fix: using cached image

--- lib/pages/home/widgets/cat_image.dart
+++ lib/pages/home/widgets/cat_image.dart
@@ -1,4 +1,6 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:cat_trivia/blocs/home_bloc/home_bloc.dart';
+import 'package:cat_trivia/resources/app_colors.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -14,9 +16,24 @@ class CatImage extends StatelessWidget {
         return Expanded(
           flex: 2,
           child: state.catModel.url != ''
-              ? Image.network(
-                  state.catModel.url,
+              ? CachedNetworkImage(
                   fit: BoxFit.fitHeight,
+                  fadeInDuration: Duration.zero,
+                  fadeOutDuration: Duration.zero,
+                  imageUrl: state.catModel.url,
+                  progressIndicatorBuilder: (context, url, downloadProgress) =>
+                      Center(
+                    child: SizedBox(
+                      width: 50,
+                      height: 50,
+                      child: CircularProgressIndicator(
+                        backgroundColor: AppColors.primary5,
+                        color: AppColors.accentBlue3,
+                        value: downloadProgress.progress,
+                      ),
+                    ),
+                  ),
+                  errorWidget: (context, url, error) => const Icon(Icons.error),
                 )
               : Container(),
         );
